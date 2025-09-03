1f689c310810a86a3dc02efc1d9a6c4c4052891e
git-push-aditya <adityadubey083@gmail.com>
2025-09-03 12:51:03 +0530

    oihf0awh

--- src/pages/Auth.tsx
+++ src/pages/Auth.tsx
@@ -167,16 +167,30 @@ const Auth = ({ user, setUser }: AuthProps) => {
         <div className="relative flex justify-center items-center transition-all duration-300 ease-in-out hidden lg:block w-[62%] h-full bg-[#E4E7E6] overflow-hidden border-r-2 border-slate-500" >
             <video src="./brain.mp4" autoPlay loop muted playsInline className="absolute top-0 left-0 w-full h-full object-cover" ></video>
 
-            <div className="relative cursor-default flex flex-col justify-center items-center text-black h-full -translate-y-20">
-                <div className="flex items-center overflow-hidden">
-                    <motion.div initial={{ x: 200, scale: 1.2 }} animate={{ x: 0, scale: 1 }} transition={{ duration: 0.8, ease: easeInOut }} className="opacity-100 scale:90 authBreakPoint:scale:100"  ><LogoIcon dim="140" /></motion.div>
-                    <motion.div initial={{ x: 200, width: '0%', opacity: 0.7 }} animate={{ width: '100%', opacity: 1, x: 0 }} transition={{ duration: 1, ease: easeInOut }} className="opacity-100 overflow-x-hidden whitespace-nowrap text-[3rem] authBreakPoint:text-[4rem] font-[600] text-white text-shadow-lg">Second Brain App</motion.div>
+            <div 
+                className="relative cursor-default flex flex-col justify-center items-center text-black h-full -translate-y-20">
+                <div 
+                    className="flex items-center overflow-hidden">
+                        <motion.div 
+                            initial={{ x: 200, scale: 1.2 }} 
+                            animate={{ x: 0, scale: 1 }} 
+                            transition={{ duration: 0.8, ease: easeInOut }} 
+                            className="opacity-100 scale:90 authBreakPoint:scale:100"  >
+                                <LogoIcon dim="140" />
+                        </motion.div>
+                        <motion.div 
+                            initial={{ x: 200, width: '0%', opacity: 0.7 }} 
+                            animate={{ width: '100%', opacity: 1, x: 0 }} 
+                            transition={{ duration: 1, ease: easeInOut }} 
+                            className="opacity-100 overflow-x-hidden whitespace-nowrap text-[3rem] authBreakPoint:text-[4rem] font-[600] text-outline font-welcome text-shadow-lg">
+                                Second Brain App
+                        </motion.div>
                 </div>
                 <motion.ul
                     initial={{ opacity: 0, y: 10 }}
                     animate={{ opacity: 1, y: 0 }}
                     transition={{ duration: 1.5, ease: easeInOut }}
-                    className="list-disc list-inside space-y-2 mt-4 text-left text-xl font-[650] text-white"
+                    className="list-disc list-inside space-y-2 mt-4 text-left text-xl font-[650] text-white font-inter"
                 >
                     <li>Capture and Organize Your Thoughts Effortlessly</li>
                     <li>Access Anywhere, Anytime</li>
@@ -187,13 +201,18 @@ const Auth = ({ user, setUser }: AuthProps) => {
         </div>
         <div className="min-h-screen transition-all duration-300 ease-in-out  w-full lg:w-[38%] bg-[#F5F5F6] lg:bg-white overflow-auto cursor-default  ">
             <div className="min-h-full flex items-center justify-center">
-                <div className="w-[65%]  h-full py-8">
+                <div className="w-[65%]  h-full pb-8 ">
                     <div
-                        className="text-6xl font-[550] font-welcome text-[#080B0A] ">{authMode == "logIn" ? "Log in" : "Sign up"}
+                        className="text-[4.4rem] font-[550] font-welcome text-[#080B0A] ">
+                            {authMode == "logIn" ? "Log in" : "Sign up"}
                     </div>
                     <div
-                        className="text-md font-[550] mt-2 text-gray-600 font-welcome ">
-                        {authMode == "logIn" ? "sign in to continue oraganizing" : "Sign up and start organizing your ideas"}
+                        className="text-md font-[550] text-gray-600 font-welcome ">
+                        {
+                            authMode == "logIn" ? 
+                                "sign in to continue oraganizing" : 
+                                "Sign up and start organizing your ideas"
+                        }
                     </div>
 
 
@@ -240,9 +259,10 @@ const Auth = ({ user, setUser }: AuthProps) => {
 
                     {
                         authMode === 'signUp' && <div>
-                            <div className={`text-xl ml-1 font-[600] mt-6 text-[#080B0A] font-roboto`}> Select profile photo</div>
+                            <div className={`text-xl ml-1 font-[600] mt-6 text-[#080B0A] font-inter`}>
+                                 Select profile photo</div>
                             <div>
-                                <div className="flex justify-around mt-4 py-2 overflow-x-auto scrollbarSB-x gap-1 lg:gap-0">
+                                <div className="flex justify-around mt-2 py-1 overflow-x-auto scrollbarSB-x gap-1 lg:gap-0">
 
                                     <img src="/dp/b2.png" onClick={() => selectProfilePic("b2")} className={`${profilePicStyle} ${profilePic === "b2" ? " scale-109" : profilePic !== "" ? " opacity-50 " : ""}`} />
                                     <img src="/dp/b3.png" onClick={() => selectProfilePic("b3")} className={`${profilePicStyle} ${profilePic === "b3" ? " scale-109" : profilePic !== "" ? " opacity-50 " : ""}`} />
@@ -256,9 +276,13 @@ const Auth = ({ user, setUser }: AuthProps) => {
                         </div>
                     }
 
-                    <div className="flex justify-between  items-center text-lg mt-7 px-2">
-                        <label className="text-[#080B0A] cursor-pointer ">
-                            <input type="checkbox" checked={rememberMe} onChange={(e) => setRememberMe(e.target.checked)} className="accent-[#94a3b8] mr-4 scale-160 cursor-pointer" /> Remember me
+                    <div className="flex justify-between  items-center text-lg mt-5 px-2">
+                        <label className="text-[#080B0A] cursor-pointer  font-inter">
+                            <input 
+                                type="checkbox" 
+                                checked={rememberMe} 
+                                onChange={(e) => setRememberMe(e.target.checked)} 
+                                className="accent-[#94a3b8] mr-4 scale-160 cursor-pointer" /> Remember me
                         </label>
                     </div>
 
